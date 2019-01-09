4d7eee810d059e218841552820327e28b7b9657b
Bryan Deagle <bryandeagle@ni.com>
2019-01-09 12:41:14 -0600

    removed flask app part

--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, redirect
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return redirect('/quote.png')
--- quote.py
+++ quote.py
@@ -5,7 +5,7 @@ import os
 
 FEED_URL = 'http://feeds.feedburner.com/brainyquote/QUOTEBR'
 STATIC_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static')
-FONT = os.path.join(STATIC_DIR, 'NotoSerif.ttf')
+FONT = 'NotoSerif.ttf'
 
 
 def get_quote():
@@ -14,12 +14,12 @@ def get_quote():
             'quote': rss['entries'][0]['description'].replace('\"', '')}
 
 
-def wrap(text, font, draw, max_width):
+def wrap(text, font, drawing, max_width):
     start, last = 0, ''
     words = text.split(' ')
     for i, v in enumerate(words):
         line = ' '.join(words[start:i+1])
-        width, height = draw.textsize(line, font=font)
+        width, height = drawing.textsize(line, font=font)
         if width < max_width:
             last = line
         else:
@@ -32,26 +32,26 @@ def draw(quote, author):
     # Create image
     screen_width, screen_height = (1080, 1920)
     font = ImageFont.truetype(FONT, 72)
-    img = Image.new('RGB', (screen_width, screen_height), color='black')
-    draw = ImageDraw.Draw(img)
+    image = Image.new('RGB', (screen_width, screen_height), color='black')
+    drawing = ImageDraw.Draw(image)
     # Get heights
-    line_height = 1.4 * draw.textsize('M', font=font)[1]
+    line_height = 1.4 * drawing.textsize('M', font=font)[1]
     height = screen_height - 256 - line_height
     # Add Author
     a = '- {}'.format(author)
-    text_width, text_height = draw.textsize(a, font=font)
+    text_width, text_height = drawing.textsize(a, font=font)
     (x, y) = (screen_width - text_width) / 2, height
-    draw.text((x, y), a, font=font, fill='white')
+    drawing.text((x, y), a, font=font, fill='white')
     height -= 2 * line_height
     # Add Quote
-    w = wrap(u'\u201c{}\u201d'.format(quote), font, draw, 980)
-    for q in reversed(list(w)):
-        text_width, text_height = draw.textsize(q, font=font)
+    w = wrap(u'\u201c{}\u201d'.format(quote), font, drawing, screen_width-100)
+    for line in reversed(list(w)):
+        text_width, text_height = drawing.textsize(line, font=font)
         (x, y) = (screen_width - text_width) / 2, height
-        draw.text((x, y), q, font=font, fill='white')
+        drawing.text((x, y), line, font=font, fill='white')
         height -= line_height
 
-    return img
+    return image
 
 
 if __name__ == '__main__':
